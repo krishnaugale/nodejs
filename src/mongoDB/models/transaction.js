61c2bea9fba7c06513e753a0519665459b8dbb24
Nilesh Satpute <satputens653@gmail.com>
2021-03-04 11:17:13 +0530

    add controller to closeAcc, openClosesAcc, lastActive,chnageLastActive

---
+++ src/mongoDB/models/transaction.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const transactionsSchema = new mongoose.Schema({
+    amount: String,
+    transferedOn: Date,
+    to: String,
+    from: String,
+    remark: String,
+    userId: String,
+});
+
+exports.transactions = mongoose.model("transactions", transactionsSchema);
